e205faa3c2459cfabd2e796e3ed703b52b841ace
Stephaniennesochi <ezestephanie64@gmail.com>
2024-06-20 17:25:07 +0100

    code

---
+++ index.js
@@ -0,0 +1,53 @@
+
+document.write("Hello World!");
+console.log("Hello World!");
+
+var a;
+console.log( typeof a)
+
+var a = 3;
+console.log( typeof a)
+
+var a = "Math"
+console.log( typeof a)
+
+var a = true
+console.log( typeof a)
+
+var a = null
+console.log( typeof a)
+
+var a = ["Math", "Math", "Math", "Math", "Math"];
+console.log( typeof a)
+console.log( a.length)
+
+var a = [3,4,5,6,7]
+console.log( typeof a)
+console.log( a.length)
+
+var a = new Array("a", "b", "c", "d", "e", "f", "g");
+console.log( typeof a)
+console.log( a.length)
+
+var a = 2 + 5
+console.log(a)
+console.log(typeof a)
+
+var a = "Stephanie" + 24
+console.log(a)
+console.log(typeof a)
+
+var a = "hi" + " steph"
+console.log(a)
+console.log(typeof a)
+
+
+window.alert("Welcome to my page")
+
+// This is a single line comment //
+
+/* This 
+    is 
+    a Multi-line 
+    comment 
+*/
