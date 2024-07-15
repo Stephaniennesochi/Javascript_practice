55be309c03269ba563b43365deb3d5a97195ad94
Eze Stephanie Nnesochi <ezestephanie64@gmail.com>
2024-07-15 20:50:03 +0100

    Add files via upload

---
+++ Javascript_Learning/index.js
@@ -0,0 +1,169 @@
+
+document.write("Hello World!");
+console.log("Hello World!");
+
+document.write(" I'm currently learning Javascript")
+var a;
+console.log( typeof a)
+
+var a = 3;
+console.log( typeof a)
+
+var a = "Math"
+console.log( typeof a)
+
+var a = true
+console.log( typeof a)
+
+var a = null
+console.log( typeof a)
+
+var a = ["Math", "Math", "Math", "Math", "Math"];
+console.log( typeof a)
+console.log( a.length)
+
+var p = ["English", "French", "Igbo", "Spanish", "German", "Italian"]
+console.log( typeof p)
+console.log( p.length)
+
+var a = [3,4,5,6,7]
+console.log( typeof a)
+console.log( a.length)
+
+var a = new Array("a", "b", "c", "d", "e", "f", "g");
+console.log( typeof a)
+console.log( a.length)
+
+var b = new Array("i", "ii", "iii", "iv", "v", "vi", "vii");
+console.log( typeof b)
+console.log( b.length)
+
+var a = 2 + 5
+console.log(a)
+console.log(typeof a)
+
+var p;
+p = 24 + "24"
+console.log(p)
+
+var f;
+f = 64 + "24"
+console.log(f)
+
+var b;
+b = 15 + 15;
+console.log(b)
+
+var b;
+b = 30 - 20;
+console.log(b)
+
+var b;
+b = 30 / 15;
+console.log(b)
+
+var b;
+b = 50 * 2;
+console.log(b)
+
+var b =30;
+b++;
+console.log(b)
+
+var a = "Stephanie" + 24
+console.log(a)
+console.log(typeof a)
+
+var a = "hi" + " steph"
+console.log(a)
+console.log(typeof a)
+
+var p;
+p = ("I'm Stephanie, " + 20 +"years old," + " my Reg no is GOU/U21/CSC/690")
+console.log(p)
+
+let colors = ["White ", "Black" , "Red" ,"Green", "Orange", "Purple", "Yellow", "Blue"]
+console.log(colors)
+console.log(colors.length)
+
+var f = 24;
+age = f + 6
+console.log(age)
+
+var f = age + 15;
+f = 12;
+age = f++
+console.log(f)
+
+var f;
+b = 20
+f = b + b;
+console.log(f)
+
+var p;
+k = 20;
+k++;
+p = k + k;
+console.log(p)
+
+var f = 55 * 5 / 5 + 5
+console.log(f)
+
+var p = 4;
+p++;
+console.log(p)
+
+var f = 6*6;
+p = f + 7 - 9;
+console.log(p)
+
+var b = 5-5+1;
+console.log(b)
+
+var d;
+var h;
+d=25;
+h=35
+k= d + h + 10;
+console.log(k)
+
+var w, k, r;
+w = 10, k = 3, r = 55;
+d = w+k-r
+console.log(d)
+
+var h;
+h= 34;
+r = h + 124;
+console.log(r)
+
+var r = 24;
+r++;
+console.log(r)
+
+console.log("I love God")
+console.log("My Favourite meal is Spagetti")
+
+
+var animals =["ram", "goat", "pig", "cow", "monkey", "chicken"]
+console.log(animals)
+console.log(typeof animals)
+console.log(animals.length)
+
+console.log('Javascript is a scripting language')
+console.log("Javascript framework(React) is available")
+
+var f = 200 + 150;
+f = 20;
+f++;
+console.log(f)
+
+console.log("My Project")
+
+// This is a single line comment //
+
+/* This 
+    is 
+    a Multi-line 
+    comment 
+*/
