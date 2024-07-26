7bf56772517aae3e92f4ea64aaaabd9ed503606b
Eze Stephanie Nnesochi <ezestephanie64@gmail.com>
2024-07-26 16:35:07 +0100

    Add files via upload
    Javascript

--- index.js
+++ index.js
@@ -406,3 +406,18 @@ console.log(num)
 var num = 100;
 num /= 2 * 8 - 15;
 console.log(num)
+
+
+var a = 10;
+var b = 5;
+var c = 12;
+var e = 8;
+var d;
+d = parseInt((a * (c - b) / e + (b + c)) <= (e * (c + a) / (b + c) + a));
+console.log(d);
+
+if (d == 1){
+console.log((a * (c - b) / e + (b + c)));
+} else {
+console.log((e * (c + a) / (b + c) + a));
+}
