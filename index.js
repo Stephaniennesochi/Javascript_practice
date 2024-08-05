fc158703079d064aab34877317f4c82ec773ca71
Eze Stephanie Nnesochi <ezestephanie64@gmail.com>
2024-08-05 15:41:00 +0100

    Add files via upload
    Javascript

--- index.js
+++ index.js
@@ -475,3 +475,27 @@ console.log(myVar)
 console.log(typeof(myVar))
 
 
+var currentDay = "Monday"
+switch(currentDay){
+
+    case "Sunday":
+        console.log("Today is Sunday")
+        break;
+    case "Monday":
+        console.log("Today is Monday")
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday")
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday")
+        break;
+    case "Thursday":
+        console.log("Today is Thursday")
+        break;
+    case "Friday":
+        console.log("Today is Friday")
+        break;
+    case "Saturday":
+        console.log("Today is Saturday")
+}
